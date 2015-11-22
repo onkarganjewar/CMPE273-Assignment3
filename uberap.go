00fbd9009c6f418b6e799859de33663e9c9faa4d
Onkar Ganjewar <ganjewaronkar@gmail.com>
2015-11-22 04:29:02 -0800

    outputs updated

--- uberap.go
+++ uberap.go
@@ -181,7 +181,7 @@ func Post_trip(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 			fmt.Println("Inside the Matrix")		}
 
 
-//program is not able to reach this statement
+
 fmt.Println("Out of the Matrix")
 	}
 	fmt.Println("Neither here")
@@ -431,7 +431,8 @@ func main() {
 	}
 	server.ListenAndServe()
 }
-/*
+/* Outputs Taken at different times of the day
+
 curl -H "Content-Type: application/json" -X POST -d '{"starting_from_location_id": "56517ac40a956e220c54cc26","location_ids" : [ "565030b20a956e1034831e08", "565030560a956e1034831e07", "5631caf90a956e12440ccdaa", "564d93b90a956e295825d7f4" ]
 }' http://localhost:3021/trips
   % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
@@ -447,7 +448,12 @@ $ curl -H "Content-Type: application/json" -X POST -d '{"starting_from_location_
 100   484  100   294  100   190     10      6  0:00:31  0:00:28  0:00:03    26{"id":"12378","status":"Planning","starting_from_location_id":"564e7e740a956e18f0fc6ae7","best_route_location_ids":["564d93b90a956e295825d7f4","5631caf90a956e12440ccdaa","565030b20a956e1034831e08","565030560a956e1034831e07"],"total_uber_costs":56,"total_uber_duration":3129,"total_distance":17}
 
 
-
+*********************0428HRS****************
+curl -H "Content-Type: application/json" -X POST -d '{"starting_from_location_id": "564e7e740a956e18f0fc6ae7","location_ids" : [ "565030b20a956e1034831e08", "565030560a956e1034831e07", "5631caf90a956e12440ccdaa", "564d93b90a956e295825d7f4" ]
+}' http://localhost:3021/trips
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100   484  100   294  100   190      9      6  0:00:32  0:00:31  0:00:01    24{"id":"12381","status":"Planning","starting_from_location_id":"564e7e740a956e18f0fc6ae7","best_route_location_ids":["564d93b90a956e295825d7f4","5631caf90a956e12440ccdaa","565030b20a956e1034831e08","565030560a956e1034831e07"],"total_uber_costs":71,"total_uber_duration":3129,"total_distance":17}
 
 
 */
